86da94cf0931e3c0ff37ad7e46f21861bbe440f2
Igor <victororlov174@gmail.com>
2023-09-03 19:05:33 +0300

    server init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+const PORT = config.get('port') || 5000
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    app.listen(PORT, () =>
+      console.log(`\napp has been started on port ${PORT}...`)
+    )
+  } catch (error) {
+    console.log('Server error', error.message)
+    process.exit(1)
+  }
+}
+
+start()
